e404266c599b94a6d8cbf2f8f906260578e10958
Nils-WZ-dualboot-mint <nilseckerle2001@protonmail.com>
2025-05-15 16:56:25 +0200

    add: linked_list.h

--- include/linked_list.h
+++ include/linked_list.h
@@ -1,4 +1,4 @@
-#ifndef ENTRY_H
+#ifndef LINKED_LIST_H
 #define ENTRY_H
 
 #include <stdio.h>
@@ -17,7 +17,7 @@ typedef struct Node {
  * @brief Initializes an list.
  * @return Pointer to the head of the list.
  */
-Node *init(void);
+Node *ll_init_node(void);
 
 /**
  * @brief Prepends an item to the list.
@@ -25,7 +25,7 @@ Node *init(void);
  * @param data - pointer to data
  * return - 0 if successful - otherwise 1
  */
-int prepend(Node **head, void* data);
+int ll_prepend_node(Node **head, void* data);
 
 /**
  * @brief append an item to the list.
@@ -33,7 +33,7 @@ int prepend(Node **head, void* data);
  * @param data - pointer to data
  * return - 0 if successful - otherwise 1
  */
-int append(Node **head, void* data);
+int ll_append_node(Node **head, void* data);
 
 /**
  * @brief remove a the i-th element in the list while preserving linked list conectivity, counting starts at 0.
@@ -41,7 +41,7 @@ int append(Node **head, void* data);
  * @param index - the element at this location is deleted
  * return - 0 if successful - 1 if index is out of range- otherwise 2
  */
-int remove(Node **head, int index);
+int ll_remove_node(Node **head, int index);
 
 /**
  * @brief Iterates over the list and applies a callback function to each entry.
@@ -49,7 +49,7 @@ int remove(Node **head, int index);
  * @param callback - Callback function to apply.
  * @param parameter_given_to_callback - Additional parameter for the callback.
  */
-void itterate_function(Node *head,
+void ll_itterate_function(Node *head,
 											 void *callback(void *parameter, void* data),
 											 void *parameter_given_to_callback);
 
@@ -58,6 +58,6 @@ void itterate_function(Node *head,
  * @param head - Pointer to the head of the list.
  * @return - Length of the list.
  */
-int entry_list_get_length(EntryNode *head);
+int ll_get_length(Node *head);
 
 #endif
